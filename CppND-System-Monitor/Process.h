bb7382b17c7eee2be9cd8cef51699262b0584fcf
dongyoon-wee <dongyoon.wee@gmail.com>
2019-07-21 04:13:24 +0000

    modify UI

--- CppND-System-Monitor/Process.h
+++ CppND-System-Monitor/Process.h
@@ -45,16 +45,11 @@ string Process::getProcess(){
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   " 
-					+ this->user
-					+ "   "
-					+ this->mem.substr(0,5)
-					+ "   "
-					+ this->cpu.substr(0,5)
-					+ "   "
-					+ this->upTime.substr(0,5)
-					+ "   "
-					+ this->cmd.substr(0,30)
-					+ "   ...");
+    return (this->pid+"\t "
+            +this->user+"\t\t"
+            +this->cpu.substr(0,5)+"\t  "
+            +this->mem.substr(0,5)+"\t\t"
+            +this->upTime.substr(0,5)+"\t\t "
+            +this->cmd.substr(0,30)+" ...");
 }
 
